dfc2d099334bb8c0c83d7aa3ac18a263ddd43841
Amo Moloko <u14284783@tuks.co.za>
2021-02-08 10:11:37 +0000

    constructor pattern is working

--- construtorPattern.js
+++ construtorPattern.js
@@ -2,7 +2,9 @@ const Solider = (rank,battles,divison)=>{
   
   const toString = () => `This Solider has fought in ${battles} at ${rank} in ${divison}`
 
-  return toString
+  return toString()
 }
 
-const private
+const private = Solider("Private", 30,"1st Airborn")
+
+console.log(private.toString())
